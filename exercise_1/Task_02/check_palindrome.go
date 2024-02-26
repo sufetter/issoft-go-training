af1f0cf227ec57fee1ef9ea7e00a155d40c62de8
Hiks <lobunetsdaniil@yandex.ru>
2024-02-26 20:52:14 +0300

    exercise_1 task_2 algorithm improvement

--- exercise_1/Task_02/check_palindrome.go
+++ exercise_1/Task_02/check_palindrome.go
@@ -7,22 +7,24 @@ func main() {
 	fmt.Print("Input int positive number: ")
 	fmt.Scan(&num)
 
-	ternary := ""
-	for num > 0 {
-		ternary = fmt.Sprintf("%d", num%3) + ternary
-		num /= 3
+	ternary := 0
+	factor := 1
+	numCopy := num
+
+	for numCopy > 0 {
+		ternary += (numCopy % 3) * factor
+		numCopy /= 3
+		factor *= 10
 	}
 
-	isPalindrome := true
-	numTernaryLength := len(ternary)
-	for i := 0; i < numTernaryLength/2; i++ {
-		if ternary[i] != ternary[numTernaryLength-1-i] {
-			isPalindrome = false
-			break
-		}
+	reverse := 0
+	numCopy = ternary
+	for numCopy > 0 {
+		reverse = reverse*10 + numCopy%10
+		numCopy /= 10
 	}
 
-	if isPalindrome {
+	if ternary == reverse {
 		fmt.Println("Palindrome")
 	} else {
 		fmt.Println("Fake")
