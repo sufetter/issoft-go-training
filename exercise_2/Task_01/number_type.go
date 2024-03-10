cd32f745a28cce167243f26c9be4c48c0552fff7
Hiks <lobunetsdaniil@yandex.ru>
2024-03-10 16:12:43 +0300

    exercise_2 task_1 refactor

--- exercise_2/Task_01/number_type.go
+++ exercise_2/Task_01/number_type.go
@@ -5,46 +5,25 @@ import (
 	"time"
 )
 
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	if n == 2 || n == 3 {
-		return true
-	}
-	if n%2 == 0 || n%3 == 0 {
-		return false
-	}
-	i := 5
-	w := 2
-	for i*i <= n {
-		if n%i == 0 {
-			return false
-		}
-		i += w
-		w = 6 - w
-	}
-	return true
-}
-
 func checkNumberType(n int) (string, bool) {
 	if n <= 1 {
 		return "", false
 	}
 
-	primeFactors := make(map[int]bool)
+	primeFactors := make([]int, 0)
 
 	for i := 2; i*i <= n; i++ {
 		for n%i == 0 {
-			primeFactors[i] = true
+			primeFactors = append(primeFactors, i)
 			n /= i
 		}
 	}
 
-	if isPrime(n) {
-		primeFactors[n] = true
+	if n > 1 {
+		primeFactors = append(primeFactors, n)
 	}
 
+	fmt.Print(primeFactors)
 	if len(primeFactors)%2 == 0 {
 		return "even", true
 	}
@@ -52,21 +31,17 @@ func checkNumberType(n int) (string, bool) {
 }
 
 func main() {
-	//Данный алгоритм менялся много раз и я не уверен,
-	//что на данный момент он работает лучшим образом,
-	//но текущая сложность O(sqrt(n))
 	testCases := [...]int{
-		5,
-		-3,
-		2,
-		10,
-		-1,
+		9,
+		3,
+		15,
+		30,
+		63018038201,
 	}
-
 	s := time.Now()
 	for _, tc := range testCases {
 		numType, flag := checkNumberType(tc)
 		fmt.Printf("Result: %v, %v\n", numType, flag)
 	}
-	fmt.Printf("%v", time.Since(s))
+	fmt.Printf("Time elapsed: %v\n", time.Since(s))
 }
